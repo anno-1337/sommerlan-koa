883b25dba5d99db709446911985e75c384cb297c
vegfjogs <vegard.fjogstad@finn.no>
2018-06-18 23:36:19 +0200

    Add html from site

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,2 @@
 import './index.html';
-import './main.css'
-
-const div = document.createElement('div');
-div.style.width = '100px';
-div.style.height = '100px';
-div.style.background = 'red';
-div.style.color = 'white';
-div.innerHTML = 'Watching for changes client-side-rendered my friend';
-
-document.getElementById('main').appendChild(div);
+import './main.css';
